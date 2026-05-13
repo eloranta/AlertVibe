742c31d59e518147806d9e6e1e549180cf79860b
hannu <hannu.eloranta@gmail.com>
2026-05-13 16:22:24 +0300

    Parse WSJT UDP messages

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,10 +2,20 @@
 
 #include <QMainWindow>
 
+class QUdpSocket;
+
 class MainWindow final : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+
+private slots:
+    void readPendingDatagrams();
+
+private:
+    void parseMessage(QByteArray buffer);
+
+    QUdpSocket *udpSocket = nullptr;
 };
